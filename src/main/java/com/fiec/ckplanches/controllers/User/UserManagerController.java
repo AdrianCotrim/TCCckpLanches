3cda6d1979acef13d45cfda275fbcca5f70ca117
LuisGustavoDev <luisgustavodev@hotmail.com>
2024-09-16 16:56:17 -0300

    Criando EndPoint UserManager

--- src/main/java/com/fiec/ckplanches/controllers/User/UserManagerController.java
+++ src/main/java/com/fiec/ckplanches/controllers/User/UserManagerController.java
@@ -2,13 +2,22 @@ package com.fiec.ckplanches.controllers.User;
 
 import com.fiec.ckplanches.DTO.SupplyTableDTO;
 import com.fiec.ckplanches.DTO.UserTableDTO;
+import com.fiec.ckplanches.model.product.Product;
 import com.fiec.ckplanches.model.supply.Supply;
 import com.fiec.ckplanches.model.user.User;
 import com.fiec.ckplanches.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +44,30 @@ public class UserManagerController {
             UserDTOs.add(UserDTO);
         }
         return UserDTOs;
+    }
+
+    @PostMapping
+    @Secured("ADMIN")
+    public User criarUsuario(@RequestBody User usuario) {
+        User usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    @PutMapping
+    @Secured("ADMIN")
+    public User editarUsuario(@RequestBody User user) {
+        User userNovo = dao.save(user);
+        return userNovo;
+    }
+
+    @DeleteMapping("/{id}")
+    @Secured("ADMIN")
+    public void deletarUsuario(@PathVariable Integer id) {
+        if (dao.existsById(id)) {
+            dao.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado");
+        }
     }
 }
 
