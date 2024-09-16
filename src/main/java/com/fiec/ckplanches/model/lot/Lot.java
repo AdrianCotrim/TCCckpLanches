4a1618fe01633b2652f32e3fc11ec22294c5e441
Eder-Moraes <moraeseder880@gmail.com>
2024-09-16 16:36:37 -0300

    Ajustes

--- src/main/java/com/fiec/ckplanches/model/lot/Lot.java
+++ src/main/java/com/fiec/ckplanches/model/lot/Lot.java
@@ -1,17 +1,16 @@
 package com.fiec.ckplanches.model.lot;
 
 import java.sql.Date;
-import java.util.List;
 
 import com.fiec.ckplanches.model.supply.Supply;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -32,6 +31,7 @@ public class Lot {
     @Column(name = "data_validade", nullable = false)
     private Date expiration_date;
 
-    @OneToMany(mappedBy = "lot", cascade = CascadeType.ALL)
-    private List<Supply> supplies;
+    @ManyToOne
+    @JoinColumn(name = "fk_lote", nullable = true)
+    private Supply supply;
 }
