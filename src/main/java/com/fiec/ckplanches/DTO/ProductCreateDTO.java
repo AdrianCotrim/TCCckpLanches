431313cbdc45f139a6583100ad26f4dd31cbdfbb
Eder-Moraes <moraeseder880@gmail.com>
2024-09-30 17:25:34 -0300

    Ajustes finais de hoje adicionar produto

--- src/main/java/com/fiec/ckplanches/DTO/ProductCreateDTO.java
+++ src/main/java/com/fiec/ckplanches/DTO/ProductCreateDTO.java
@@ -1,12 +1,17 @@
 package com.fiec.ckplanches.DTO;
 
+import java.util.List;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import com.fiec.ckplanches.model.supply.Supply;
 
 public record ProductCreateDTO(
     String productName,
     Double productValue,
     MultipartFile imagem, // Arquivo da imagem
-    String description
+    String description,
+    List<Supply> supplies 
 ) {
     
 }
