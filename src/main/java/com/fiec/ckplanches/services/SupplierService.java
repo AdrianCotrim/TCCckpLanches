4063f6cb8fc301b5e80434ad015aae404a5d549e
Eder-Moraes <moraeseder880@gmail.com>
2024-11-03 18:29:05 -0300

    Ajuste listarFornecedores do SupplierService

--- src/main/java/com/fiec/ckplanches/services/SupplierService.java
+++ src/main/java/com/fiec/ckplanches/services/SupplierService.java
@@ -21,6 +21,9 @@ public class SupplierService {
 
     public List<SupplierTableDTO> listarFornecedores(List<Supplier> suppliers){
         List<SupplierTableDTO> supplierTableDTOs = new ArrayList<>();
+        for (Supplier supplier : suppliers) {
+            supplierTableDTOs.add(convertSupplierToTableDTO(supplier));
+        }
         return supplierTableDTOs;
     }
 
