65b0054610a4b6a309663d8a7647c745301e5b4f
Eder-Moraes <moraeseder880@gmail.com>
2024-11-03 10:34:08 -0300

    Adicionando as mensagens de erro do login

--- src/main/java/com/fiec/ckplanches/controllers/AuthController.java
+++ src/main/java/com/fiec/ckplanches/controllers/AuthController.java
@@ -3,12 +3,18 @@ package com.fiec.ckplanches.controllers;
 import com.fiec.ckplanches.DTO.AuthenticationDTO;
 import com.fiec.ckplanches.DTO.LoginResponseDTO;
 import com.fiec.ckplanches.DTO.RegisterDTO;
+import com.fiec.ckplanches.model.enums.Status;
+import com.fiec.ckplanches.model.user.StatusConta;
 import com.fiec.ckplanches.model.user.User;
 import com.fiec.ckplanches.repositories.UserRepository;
 import com.fiec.ckplanches.services.TokenService;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -27,12 +33,34 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody AuthenticationDTO login) {
-        var usernamePassowrd = new UsernamePasswordAuthenticationToken(login.username(), login.userPassword());
-        var auth = this.authenticationManager.authenticate(usernamePassowrd);
-
-        var token = tokenService.generateToken((User)auth.getPrincipal());
-
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+        try{
+            Optional<User> userOptional = repository.findByUserNameReturnUser(login.username());
+    
+            // Verifica se o usuário existe
+    
+            if(userOptional.isEmpty()) return ResponseEntity.notFound().build();
+    
+            User user = userOptional.get();
+    
+            // Verifica se ele está ativo
+    
+            if(user.getStatusConta() != StatusConta.ATIVO || user.getStatus() != Status.ATIVO)
+            return ResponseEntity.badRequest().body("Usuário desativado pelo administrador!");
+    
+            var usernamePassowrd = new UsernamePasswordAuthenticationToken(login.username(), login.userPassword());
+            var auth = this.authenticationManager.authenticate(usernamePassowrd);
+    
+            var token = tokenService.generateToken((User)auth.getPrincipal());
+    
+            return ResponseEntity.ok(new LoginResponseDTO(token));
+    
+            } catch (BadCredentialsException e){
+                System.out.println(e);
+                return ResponseEntity.badRequest().body(e.getMessage());
+            } catch (Exception e){
+                System.out.println(e);
+                return ResponseEntity.internalServerError().body("Erro inesperado no servidor!");
+            }
     }
 
     @PostMapping("/register")
