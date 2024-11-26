aeda86bb28fd6e4f9e4d12526904b779d7b8b80d
LuisGustavoDev <145783586+LuisGustavoDev@users.noreply.github.com>
2024-11-26 11:37:01 -0300

    Delete PycharmProjects directory

--- PycharmProjects/pythonProject/Desafio60Fatorial.py
+++
@@ -1,7 +0,0 @@
-fatorial = int(input('Insira um número para saber seu fatorial: '))
-contador1 = 1
-contador2 = 2
-while contador2 <= fatorial:
-   contador1 = contador1 * contador2
-   contador2 = contador2 + 1
-print('o fatorial de {} é contador1'.format(fatorial))
--- PycharmProjects/pythonProject/desafio57SexoMasculinoFeminino.py
+++
@@ -1,8 +0,0 @@
-sexo = str(input('Informe seu sexo: [M/F]')).upper()
-while sexo not in 'MF':
-    sexo = str(input('Dados inválidos. Por favor insira novamente: [M/F]')).upper()
-if sexo == 'F':
-    print('Seu sexo é Feminino')
-else:
-    print('Seu sexo é Masculino')
-
